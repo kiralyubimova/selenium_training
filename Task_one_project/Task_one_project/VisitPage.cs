2758902f1b3881a6e297fc29c407c49262db909e
Kira Liubimova <kira.lyubimova@gmail.com>
2018-03-11 23:48:46 +0300

    Задание 1

---
+++ Task_one_project/Task_one_project/VisitPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Task_one_project
+{
+    [TestFixture]
+    public class VisitPage
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void GoToPage()
+        {
+            driver.Url = "https://en.wikipedia.org/wiki/Main_Page";
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
